3355f98ada1a54d0d1562ecb092cd8454c69ada5
ConraLev <134116838+ConraLev@users.noreply.github.com>
2023-06-10 13:51:57 -0300

    Add files via upload

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,31 +3,52 @@ const nombre = document.getElementById("name");
 const mail = document.getElementById("correo");
 const mensajes = document.getElementById("comentario");
 const form = document.getElementById("form");
+const errores = document.getElementById("errors");
 
 
  form.addEventListener("submit", e=>{
     e.preventDefault()
     let regexEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-
+    let ok = false
+    let error= ""
+    errors.innerHTML= ""
 
       if(nombre.value.length <4){
-        alert("Debe ingresar nombre completo");
+        error += 'Debe ingresar nombre completo<br>';
+        ok = true
+      }
 
+      if(nombre.value.length >20){
+        error += 'El nombre ingresado no es valido<br>';
+        ok = true
       }
 
       if(!regexEmail.test(mail.value)){
-        alert("El mail ingresado no es valido");
-
+        error +="El mail ingresado no es valido<br>";
+        ok = true
       }
 
       if(mensajes.value.length <5){
-        alert("El mensaje es muy corto");
-  
+        error +="El mensaje es muy corto<br>";
+        ok = true
       } 
       
       if(mensajes.value.length >500){
-        alert("El mensaje es muy largo");
+        error +="El mensaje es muy largo<br>";
+        ok = true
       }
+
+      if(ok){
+        document.getElementById("form_error").style.display="block";
+        document.getElementById("form_msj").style.display="none"
+        errors.innerHTML = error
+      }
+
+       if(!ok){
+        document.getElementById("form_error").style.display = "none";
+        document.getElementById("form_msj").style.display= "block";
+      } 
+       
 
     })
  
